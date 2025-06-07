69eb9d98856cd41c347615884cf3b4d1aa568cea
시아 <2237043@pcu.ac.kr>
2025-06-07 18:04:27 +0900

    1차 수정

--- README.md
+++
@@ -1,3 +0,0 @@
-# ddaengchuri
-
-[React Native](https://reactnative.dev/)로 개발되는 음식물 쓰레기 절감을 위한 마감할인 엡 소스코드 프로젝트.
--- reanimated.config.js
+++
@@ -1,6 +0,0 @@
-// reanimated.config.js
-module.exports = {
-  enableDebugger: false,
-  enableBabelPlugin: true,
-  warnAboutReanimatedUsage: false, // ✅ 이 옵션이 경고 숨깁니다
-};
